bc1ed8923f9f327ccaedfac5ab409d6f113707ab
Nx0dle <ramenkeebs@proton.me>
2024-01-09 20:32:22 +0100

    add favicon and style flash messages

--- app/controllers/stock_tracker_controller.rb
+++ app/controllers/stock_tracker_controller.rb
@@ -1,4 +1,5 @@
 class StockTrackerController < ApplicationController
   def index
   end
+
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import { Turbo } from "@hotwired/turbo-rails"
-Turbo.session.drive = false
-
+import '@hotwired/turbo-rails'
 import "controllers"
 import 'bootstrap'
 import 'jquery'
 import "@popperjs/core"
-
-const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
-const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl))
