913239886baacf65cb72737dc8997ed069c97244
Nx0dle <ramenkeebs@proton.me>
2024-01-14 10:28:28 +0100

    add remove stock tracking

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -9,4 +9,11 @@ class UserStocksController < ApplicationController
     flash[:notice] = "Stock #{stock.name} was added to your portfolio"
     redirect_to my_portfolio_path
   end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    UserStock.where(stock: stock, user: current_user.id).first.destroy
+    flash[:notice] = "#{stock.ticker} tracking removed"
+    redirect_to my_portfolio_path
+  end
 end
